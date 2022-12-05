65cf6f84762bddf6a50dae4221ea0894375044a8
JenniKate Wallace <jennikate@gmail.com>
2022-12-05 15:43:03 +0000

    Setup routing for registration pages

--- src/pages/Register/RegisterEmailAddress.jsx
+++ src/pages/Register/RegisterEmailAddress.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
 const RegisterEmailAddress = () => {
 
   return (
-    <h1>Email address page</h1>
+    <>
+      <h1>Email address page</h1>
+      <Link to="/create-account/your-details">next page</Link>
+    </>
   );
 };
 
